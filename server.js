5660cec1e7fe8f722388aea5790697d77c2c236f
FDMcreative <federico.delmonte@gmail.com>
2018-12-26 18:05:57 +0100

    chenges for heroku deploy

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/FDMcoding'));
-//
-// app.get('/*', function(req,res) {
-//
-// res.sendFile(path.join(__dirname+'/dist/FDMcoding/index.html'));
-// });
-//
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
+//Install express server
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/FDMcoding'));
 
+app.get('/*', function(req,res) {
 
+res.sendFile(path.join(__dirname+'/dist/FDMcoding/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+// app.use(express.static(__dirname + '/dist/'));
+// app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+//
+//
+// app.listen(process.env.PORT || 8080);
